23f3063f9c9fc56c5ff68070e5d56ebfa2611ba3
moonhee0507 <sjmh0507@naver.com>
2022-07-06 14:58:29 +0900

    ✨ 이메일로그인 및 회원가입 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import reset from "styled-reset";
 import Start from './pages/Start';
 import Login from './pages/Login';
 import Home from './pages/Home';
-import CreateAccount from './pages/Join';
+import EmailLogin from './pages/EmailLogin';
+import Join from './pages/Join';
 
 const Globalstyle = createGlobalStyle`
-  ${reset}
+    ${reset}
 `
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Globalstyle />
-        <Switch>
-          <Route path="/" exact component={Start}/>
-          <Route path="/home" component={Home}/>
-          <Route path="/login" component={Login}/>
-          <Route path="/createaccount" component={CreateAccount}/>
-        </Switch>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Globalstyle />
+                <Switch>
+                    <Route path="/" exact component={Start}/>
+                    <Route path="/home" component={Home}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path='/emaillogin' component={EmailLogin} />
+                    <Route path='/join' component={Join} />
+                </Switch>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
