d1cb6cd789a74823c8b29302d57329a18e8e16b1
moonhee0507 <sjmh0507@naver.com>
2022-07-06 15:00:01 +0900

    ✨ 버튼 컴포넌트 구현

---
+++ src/components/login/Button.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from 'styled-components';
+import LogoKakao from '../../assets/kakao-logo.png';
+import LogoGoogle from '../../assets/google-logo.png';
+import LogoFacebook from '../../assets/facebook-logo.png';
+
+const StyledButton = styled.button`
+box-sizing: border-box;
+    width: 100%;
+    border: 1px solid ${(props) => (
+        props.account == 'kakao' && '#F2C94C') 
+        || (props.account == 'google' && '#767676;') 
+        || (props.account == 'facebook' && '#2D9CDB;')
+    };
+    border-radius: 44px;
+    background-color: white;
+    padding: 12px 86px;
+
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 18px;
+    color: #767676;
+    position: relative;
+
+    background-image: url(${(props) => (
+        props.account == 'kakao' && `${LogoKakao}`) 
+        || (props.account == 'google' && `${LogoGoogle}`) 
+        || (props.account == 'facebook' && `${LogoFacebook}`)
+    });
+    background-repeat: no-repeat;
+    background-size: 24px 24px;
+    background-position: 14px 10px;
+    word-break: keep-all;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    
+    @media (max-width: 768px) {
+        padding: 12px 50px
+    }
+    
+    @media (max-width: 540px) {
+    }
+`;
+
+function Button(props) {
+    return (
+        <StyledButton account={props.account} disabled={props.disabled} style={props.style}>{props.text}</StyledButton>
+    );
+}
+
+export default Button;
