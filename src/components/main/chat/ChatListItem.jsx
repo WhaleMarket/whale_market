ba1361d01943e2cb103746961f3dbdb1866eb333
moonhee0507 <sjmh0507@naver.com>
2022-07-07 16:17:46 +0900

    🔥 채팅날짜 스타일드 컴포넌트 주석 제거

--- src/components/main/chat/ChatListItem.jsx
+++ src/components/main/chat/ChatListItem.jsx
@@ -47,13 +47,7 @@ const ChatDate = styled.p`
     font-size: 12px;
     line-height: 13px;
     color: #DBDBDB;
-    /* position: relative;
-    bottom: 0;
-    right: 0; */
-    /* display: flex;
-    align-items: flex-end; */
     margin-top: 24px;
-    /* margin-left: 10px; */
 `;
 
 function ChatListItem(props) {
