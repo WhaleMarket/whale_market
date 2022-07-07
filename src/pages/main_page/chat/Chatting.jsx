d24c8f57af47dbc6fb1224f865f0a30be191b815
moonhee0507 <sjmh0507@naver.com>
2022-07-07 21:36:48 +0900

    ✏️ 작은 따옴표로 수정 및 콘솔로그 제거

--- src/pages/main_page/chat/Chatting.jsx
+++ src/pages/main_page/chat/Chatting.jsx
@@ -1,12 +1,12 @@
-import { useHistory } from "react-router-dom";
-import ChatProfileHeader from "../../../components/main/chat/ChatProfileHeader";
-import ChatList from "../../../components/main/chat/ChatList";
-import chatData from "../../../components/main/chat/chatData.json";
+import { useHistory } from 'react-router-dom';
+import ChatProfileHeader from '../../../components/main/chat/ChatProfileHeader';
+import ChatList from '../../../components/main/chat/ChatList';
+import chatData from '../../../components/main/chat/chatData.json';
 
 function Chatting(props) {
     const {} = props;
     const history = useHistory();
-    console.log('props', props)
+
     return(
         <>
             <ChatProfileHeader />
