3d36441ba4b3f6f5d1f7a8e798f48959ea4e7dad
moonhee0507 <sjmh0507@naver.com>
2022-07-07 20:06:56 +0900

    ✨ Chatting 및 ChattingView 페이지 컴포넌트 라우트 추가

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/main/Navbar";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Chatting from "../pages/main_page/chat/Chatting";
+import ChattingView from "../pages/main_page/chat/ChattingView";
 import MainProfile from "../pages/main_page/MainProfile";
 import Home from "../pages/main_page/Home";
 import Posting from "../pages/main_page/Posting";
@@ -12,7 +13,8 @@ function Main(){
                 <Navbar />
                 <Switch>
                     <Route path="/home" exact component={Home}/>
-                    <Route path="/chatting" component={Chatting}/>
+                    <Route path="/chatting" exact component={Chatting}/>
+                    <Route path="/chatting/:chatId" component={ChattingView}/>
                     <Route path="/profile" component={MainProfile}/>
                     <Route path="/posting" component={Posting}/>
                 </Switch>
