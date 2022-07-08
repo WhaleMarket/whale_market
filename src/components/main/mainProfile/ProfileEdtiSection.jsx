3f8f3cea3d860e87736ff31355a0860d9639d8e2
soheeyeo <sohee407@gmail.com>
2022-07-09 02:19:53 +0900

    :fire: ProfileEditContainer padding 값 삭제

--- src/components/main/mainProfile/ProfileEdtiSection.jsx
+++ src/components/main/mainProfile/ProfileEdtiSection.jsx
@@ -6,7 +6,6 @@ const ProfileEditContainer = styled.section`
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding: 1.875rem 2.125rem 19.625rem;
 `
 
 function ProfileEditSection() {
