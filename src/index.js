24143e26e42b4d7beb02e15c731d174c74b4f82b
moonhee0507 <sjmh0507@naver.com>
2022-07-09 02:52:55 +0900

    🔥 useContext로 AuthProvider 사용을 위해 userContext 삭제

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { UserProvider } from './context/userContext';
 import { AuthProvider } from './context/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
