1e672bb658c3db5b57aef588557f3957c42c97a6
soheeyeo <sohee407@gmail.com>
2022-07-09 00:22:02 +0900

    :lipstick: Follow Button 컴포넌트 구현

---
+++ src/components/main/mainProfile/FollowButton.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FollowBtn = styled.button`
+    padding: 0.5rem 2.5rem;
+    border-style: none;
+    border-radius: 1.875rem;
+    background-color: #00BCD4;
+    color: #fff;
+    font-size: 0.875em;
+    font-weight: 500;
+    cursor: pointer;
+`
+
+const UnFollowBtn = styled.button`
+    padding: 0.5rem 2.125rem;
+    border: 0.063rem solid #DBDBDB;
+    border-radius: 1.875rem;
+    background-color: #fff;
+    color: #767676;
+    font-size: 0.875em;
+    font-weight: 500;
+    cursor: pointer;
+`
+
+function FollowButton() {
+    return(
+        <>
+        <FollowBtn type="submit">
+            팔로우
+        </FollowBtn>
+        <UnFollowBtn type="submit">
+            언팔로우
+        </UnFollowBtn>
+        </>
+    )
+}
+
+export default FollowButton;
