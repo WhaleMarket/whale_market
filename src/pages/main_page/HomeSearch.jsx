e1e3dd4e9a1f71e2e0a9c4fb5ef0c9b446bed366
“nimoseel” <fromsomini@gmail.com>
2022-07-08 14:05:36 +0900

    :lipstick: 계정검색화면 구현

---
+++ src/pages/main_page/HomeSearch.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchBar from '../../components/main/homeSearch/SearchBar';
+import Navbar from '../../components/main/Navbar';
+
+function HomeSearch(){
+    return(
+        <>
+            <SearchBar />
+            <Navbar />
+        </>
+    );
+}
+
+export default HomeSearch;
