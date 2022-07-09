ae13ffef30d336d92945a0ae79a7fd77dc07d21d
Lim Heelae <ehhwll@hanmail.net>
2022-07-10 01:27:39 +0900

    :sparkles: Posting 페이지에서 Navbar가 나오지 않도록 구현

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -1,24 +1,34 @@
 import Navbar from "../components/main/Navbar";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Chatting from "../pages/main_page/Chatting";
 import MainProfile from "../pages/main_page/MainProfile";
 import Home from "../pages/main_page/Home";
 import Posting from "../pages/main_page/Posting";
+import { useLocation } from "react-router-dom";
 
-function Main(){
-    return(
-        <>
-            <BrowserRouter basename="/main">
-                <Navbar />
-                <Switch>
-                    <Route path="/home" exact component={Home}/>
-                    <Route path="/chatting" component={Chatting}/>
-                    <Route path="/profile" component={MainProfile}/>
-                    <Route path="/posting" component={Posting}/>
-                </Switch>
-            </BrowserRouter>
-        </>
-    );
+function Test() {
+  const location = useLocation();
+  return (
+    <>
+      {location.pathname !== "/posting" && <Navbar />}
+      <Switch>
+        <Route path="/home" exact component={Home} />
+        <Route path="/chatting" component={Chatting} />
+        <Route path="/profile" component={MainProfile} />
+        <Route path="/posting" component={Posting} />
+      </Switch>
+    </>
+  );
+}
+
+function Main() {
+  return (
+    <>
+      <BrowserRouter basename="/main">
+        <Test />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default Main;
