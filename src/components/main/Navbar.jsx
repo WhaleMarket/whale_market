13479f37b23f4035c4058e0b7152d8132e1bcb00
soheeyeo <sohee407@gmail.com>
2022-07-10 15:20:00 +0900

    :truck: NavBar Profile 경로 변경

--- src/components/main/Navbar.jsx
+++ src/components/main/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = ({location})=>{
             <List className='home' current={location.pathname === '/home'}><Link to='/home'><Logo className="Home" src={location.pathname === '/home' ? home_icon_fill : home_icon} alt="home icon"/>홈</Link></List>
             <List className='chatting' current={location.pathname === '/chatting'}><Link to='/chatting'><Logo className="Home" src={location.pathname === '/chatting' ? chatting_icon_fill : chatting_icon} alt="home icon"/>채팅</Link></List>
             <List className='posting' current={location.pathname === '/posting'}><Link to='/posting'><Logo className="Home" src={posting_icon} alt="home icon"/>게시물 작성</Link></List>
-            <List className='profile' current={location.pathname === '/profile'}><Link to='/profile'><Logo className="Home" src={location.pathname === '/profile' ? user_icon_fill : user_icon} alt="home icon"/>프로필</Link></List>
+            <List className='profile' current={location.pathname === '/mainprofile'}><Link to='/mainprofile'><Logo className="Home" src={location.pathname === '/mainprofile' ? user_icon_fill : user_icon} alt="home icon"/>프로필</Link></List>
         </Bottomnav>
     )
 }
