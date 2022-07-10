5077f74bcd9a6edb88fe9aacb5dd8be4a607b29d
moonhee0507 <sjmh0507@naver.com>
2022-07-10 17:47:37 +0900

    ✨ 프로필수정 컴포넌트 구현

---
+++ src/pages/main_page/mainProfile/ProfileEdit.jsx
@@ -0,0 +1,13 @@
+import ProfileEditHeader from '../../../components/main/mainProfile/ProfileEditHeader';
+import ProfileEditSection from '../../../components/main/mainProfile/ProfileEditSection';
+
+function ProfileModification() {
+    return (
+        <>
+        <ProfileEditHeader/>
+        <ProfileEditSection/>
+        </>
+    )
+}
+
+export default ProfileModification;
