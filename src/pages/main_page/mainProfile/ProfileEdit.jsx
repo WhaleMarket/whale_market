b1c42f4272bd1ff32539d8f1f8dccabeb55a5394
soheeyeo <sohee407@gmail.com>
2022-07-10 15:24:48 +0900

    :lipstick: UserProfile, ProfileEdit 페이지 추가

---
+++ src/pages/main_page/mainProfile/ProfileEdit.jsx
@@ -0,0 +1,13 @@
+import ProfileEditHeader from '../../../components/main/mainProfile/ProfileEditHeader';
+import ProfileEditSection from '../../../components/main/mainProfile/ProfileEdtiSection';
+
+function ProfileModification() {
+    return (
+        <>
+        <ProfileEditHeader/>
+        <ProfileEditSection/>
+        </>
+    )
+}
+
+export default ProfileModification;
