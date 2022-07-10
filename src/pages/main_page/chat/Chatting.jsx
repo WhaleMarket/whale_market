7fbd8c3b0a66e86b99a2b910f9d49112a62ad4a3
moonhee0507 <sjmh0507@naver.com>
2022-07-10 21:02:56 +0900

    ✨ 채팅 및 채팅방 페이지 컴포넌트 추가로 페이지 전환기능 구현

---
+++ src/pages/main_page/chat/Chatting.jsx
@@ -0,0 +1,23 @@
+import { useHistory } from 'react-router-dom';
+import ChatProfileHeader from '../../../components/main/Chatprofileheader';
+import ChatList from '../../../components/main/chat/ChatList';
+import chatData from '../../../components/main/chat/chatData.json';
+
+function Chatting(props) {
+    const {} = props;
+    const history = useHistory();
+
+    return(
+        <>
+            <ChatProfileHeader />
+            <ChatList 
+                rooms={chatData}
+                onClickItem={(item) => {
+                    history.push(`/chatting/${item.id}`);
+                }}
+            />
+        </>
+    );
+}
+
+export default Chatting;
