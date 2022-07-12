11f1ed9ca0a3d0c187692a4f0614b318904e353a
moonhee0507 <sjmh0507@naver.com>
2022-07-12 14:57:04 +0900

    🐛 회원가입 페이지 로컬스토리지 및 다음페이지, 유저정보 전달 수정 중

--- src/pages/login_page/Join.jsx
+++ src/pages/login_page/Join.jsx
@@ -1,8 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { JoinForm } from '../../components/join/JoinForm';
+import Profile from './Profile';
 
 const Join = (props) => {
+    const [nextPage, setNextPage] = useState(true);
+    const [userInfo, setUserInfo] = useState({ email: '', password: '' });
+    const history = useHistory();
+
+    useEffect(() => {
+        if (window.localStorage.getItem("token")) {
+            history.push("/main/home");
+        }
+    }, [history]);
+
     return (
-        <JoinForm />
+        <>
+            {nextPage ? (
+                <JoinForm setNextPage={setNextPage} setUserInfo={setUserInfo} />
+                ) : (
+                <Profile userInfo={userInfo} />
+            )}
+        </>
     );
 }
 
