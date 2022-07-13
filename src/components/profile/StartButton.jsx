610236d79c41902aaf951c9af8b1e8253f185111
soheeyeo <sohee407@gmail.com>
2022-07-13 23:02:12 +0900

    :sparkles: 백그라운드 컬러, 버튼 커서 및 disabled props 기능 추가

--- src/components/profile/StartButton.jsx
+++ src/components/profile/StartButton.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StartBtn = styled.button`
+    margin-top: 2.125rem;
     padding: 0.813rem 6.688rem 0.813rem 6.75em;
     border-style: none;
     border-radius: 2.750rem;
-    background-color: #00BCD4;
+    background-color: ${(props) => props.disabled ? '#B2EBF2' : '#00BCD4'};
     color: #fff;
     font-size: 0.875em;
-    cursor: pointer;
-    :disabled {
-        background: #B2EBF2;
-    }
+    cursor: ${(props) => props.disabled ? "default" : "pointer"};
     `;
 
 function StartButton({disabled}) {
