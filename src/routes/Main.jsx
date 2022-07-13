5aa88c0dbab59b7cc78f62643bc639c14f12093f
soheeyeo <sohee407@gmail.com>
2022-07-14 00:09:26 +0900

    :truck: MyProfile 파일명 변경으로 인한 루트 수정

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -2,7 +2,7 @@ import Navbar from "../components/main/Navbar";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Chatting from "../pages/main_page/chat/Chatting";
 import ChattingView from "../pages/main_page/chat/ChattingView";
-import MainProfile from "../pages/main_page/mainProfile/UserProfile";
+import MainProfile from "../pages/main_page/mainProfile/MyProfile";
 import Home from "../pages/main_page/Home";
 import Posting from "../pages/main_page/Posting";
 import { useLocation } from "react-router-dom";
