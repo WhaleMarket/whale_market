c85b83e404c3a7610978ef76841bf365a00657db
Lim Heelae <ehhwll@hanmail.net>
2022-07-14 23:56:11 +0900

    :lipstick: 이미지 업로드 할 공간 구현

---
+++ src/components/main/posting/postingsection/PostingImg.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import CancleBtn from "./CancleBtn";
+
+const PostImage = styled.img`
+  display: block;
+  margin: 10px 0 0 15px;
+  width: 200px;
+  height: auto;
+  border-radius: 10px;
+`;
+
+function PostingImg({ src, alt }) {
+  return (
+    <>
+      <PostImage src={src} alt={alt} />
+    </>
+  );
+}
+
+export default PostingImg;
