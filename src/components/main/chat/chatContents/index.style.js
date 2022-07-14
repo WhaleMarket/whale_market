28a6145780c685ddd81e33591204bea67f5c4766
moonhee0507 <sjmh0507@naver.com>
2022-07-14 23:47:47 +0900

    🚚 채팅 컴포넌트별 분리로 인한 파일 생성 및 삭제

---
+++ src/components/main/chat/chatContents/index.style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Contents = styled.div`
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 18px;
+    padding-top: 100px;
+`;
