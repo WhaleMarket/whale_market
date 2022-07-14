2e2fdff49c225e33177ad9b5e5a1bdaa568c5b17
moonhee0507 <sjmh0507@naver.com>
2022-07-14 17:42:29 +0900

    💬 로컬스토리지 주석 추가 및 컨벤션 수정

--- src/pages/login_page/Join.jsx
+++ src/pages/login_page/Join.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { JoinForm } from '../../components/join/JoinForm';
 import Profile from './Profile';
 
@@ -8,9 +8,10 @@ const Join = (props) => {
     const [userInfo, setUserInfo] = useState({ email: '', password: '' });
     const history = useHistory();
 
+    // 로컬스토리지에 토큰이 있으면 회원가입 페이지는 메인/홈으로 스킵하는 기능
     useEffect(() => {
-        if (window.localStorage.getItem("token")) {
-            history.push("/main/home");
+        if (window.localStorage.getItem('token')) {
+            history.push('/main/home');
         }
     }, [history]);
 
