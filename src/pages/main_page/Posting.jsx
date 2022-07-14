eaa4e0621e56efa588809ad94b48c1e2980c78a8
Lim Heelae <ehhwll@hanmail.net>
2022-07-14 23:49:48 +0900

    :sparkles: 이미지 업로드를 위한 context API 구현

--- src/pages/main_page/Posting.jsx
+++ src/pages/main_page/Posting.jsx
@@ -1,15 +1,18 @@
 import ImageUploadButton from "../../components/main/posting/imageuploadbutton";
 import PostingHeader from "../../components/main/posting/postingheader";
 import PostingSection from "../../components/main/posting/postingsection";
+import { UploadImageProvider } from "../../context/UploadImageListProvider";
 import { UploadProvider } from "../../context/UploadProvider";
 
 function Posting() {
   return (
     <>
       <UploadProvider>
-        <PostingHeader />
-        <PostingSection />
-        <ImageUploadButton />
+        <UploadImageProvider>
+          <PostingHeader />
+          <PostingSection />
+          <ImageUploadButton />
+        </UploadImageProvider>
       </UploadProvider>
     </>
   );
