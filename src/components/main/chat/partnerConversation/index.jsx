70df2d3b68543553d8ca4fa63d595506926fef42
moonhee0507 <sjmh0507@naver.com>
2022-07-15 17:01:09 +0900

    💄 PartnerConversation UI 구현

--- src/components/main/chat/partnerConversation/index.jsx
+++ src/components/main/chat/partnerConversation/index.jsx
@@ -1,9 +1,16 @@
-import { Wrapper, Text } from './index.style';
+import { Wrapper, Date, MessageBox, Text, PreviewProfile, ProfileImg } from './index.style';
+import basicProfileImg from '../../../../assets/basic-profile-img.png';
 
 function PartnerConversation(props) {
     return (
         <Wrapper>
+            <PreviewProfile href="">
+                <ProfileImg src={basicProfileImg} alt="프로필사진" />
+            </PreviewProfile>
+            <MessageBox>
             <Text>{props.contents}</Text>
+            </MessageBox>
+            <Date>16:02</Date>
         </Wrapper>
     );
 }
--- src/components/main/chat/partnerConversation/index.style.js
+++ src/components/main/chat/partnerConversation/index.style.js
@@ -2,15 +2,36 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
     box-sizing: border-box;
-    width: 240px;
-    left: 70px;
-    top: 410px;
-    background: #FFFFFF;
+    position: relative;
+    margin-left: 16px;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: flex-start;
+    gap: 12px;
+`;
+
+export const PreviewProfile = styled.a`
+    width: 42px;
+    height: 42px;
+    margin-top: -10px;
+`;
+
+export const ProfileImg = styled.img`
+    width: 42px;
+    height: 42px;
+`;
+
+export const MessageBox = styled.div`
+    box-sizing: border-box;
+    /* width: 240px; */
+    max-width: 240px;
+    background: #ffffff;;
     border: 1px solid #C4C4C4;
     padding: 12px;
     border-bottom-left-radius: 20px;
-    border-top-right-radius: 20px;
     border-bottom-right-radius: 20px;
+    border-top-right-radius: 20px;
     margin-bottom: 9px;
     word-break: keep-all;
 `;
@@ -23,4 +44,16 @@ export const Text = styled.p`
     display: flex;
     align-items: flex-end;
     color: #000000;
+`;
+
+export const Date = styled.p`
+    position: absolute;
+    bottom: 3px;
+    right: 49px;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 10px;
+    line-height: 13px;
+    color: #767676;
+    margin-bottom: 9px;
 `;
