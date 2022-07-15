1802e791891045d94aa9c5d9c722e97c2af27d67
moonhee0507 <sjmh0507@naver.com>
2022-07-15 16:59:57 +0900

    💄 MyConversation UI 구현

---
+++ src/components/main/chat/myConversation/index.jsx
@@ -0,0 +1,14 @@
+import { Wrapper, Date, MessageBox, Text } from './index.style';
+
+function MyConversation() {
+    return (
+        <Wrapper>
+            <Date>16:02</Date>
+            <MessageBox>
+                <Text>TestTestTestTestTestTestTestTestTestTestTest</Text>
+            </MessageBox>
+        </Wrapper>
+    );
+}
+
+export default MyConversation;
