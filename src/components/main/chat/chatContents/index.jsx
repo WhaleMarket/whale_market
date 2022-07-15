fbc4254ac71c28f18b206e03292861f2f174b3b0
moonhee0507 <sjmh0507@naver.com>
2022-07-15 16:41:41 +0900

    ✨ MyConversation 컴포넌트 추가

--- src/components/main/chat/chatContents/index.jsx
+++ src/components/main/chat/chatContents/index.jsx
@@ -1,5 +1,6 @@
 import { Contents } from './index.style';
 import PartnerConversation from '../partnerConversation/index.jsx';
+import MyConversation from '../myConversation/index.jsx';
 
 function ChatContents(props) {
     return (
@@ -7,6 +8,7 @@ function ChatContents(props) {
             <PartnerConversation contents={props.contents[0]} />
             <PartnerConversation contents={props.contents[1]} />
             <PartnerConversation contents={props.contents[2]} />
+            <MyConversation />
         </Contents>
     );
 }
