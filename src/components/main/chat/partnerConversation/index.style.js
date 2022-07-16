d5582485a5ed8cdf46dbb19c2259fe659445b408
moonhee0507 <sjmh0507@naver.com>
2022-07-17 03:50:54 +0900

    💄 대화상대 텍스트박스 UI 구현 레이아웃 수정

--- src/components/main/chat/partnerConversation/index.style.js
+++ src/components/main/chat/partnerConversation/index.style.js
@@ -2,19 +2,16 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
     box-sizing: border-box;
-    position: relative;
     margin-left: 16px;
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: flex-start;
-    gap: 12px;
+    margin-bottom: 15px;
 `;
 
 export const PreviewProfile = styled.a`
+    display: inline-block;
+    vertical-align: top;
+    margin-top: -15px;
     width: 42px;
     height: 42px;
-    margin-top: -10px;
 `;
 
 export const ProfileImg = styled.img`
@@ -23,16 +20,19 @@ export const ProfileImg = styled.img`
 `;
 
 export const MessageBox = styled.div`
+    display: inline-block;
+    margin-left: 5px;
+    margin-right: 3px;
     box-sizing: border-box;
     /* width: 240px; */
     max-width: 240px;
-    background: #ffffff;;
+    background: #ffffff;
     border: 1px solid #C4C4C4;
     padding: 12px;
     border-bottom-left-radius: 20px;
     border-bottom-right-radius: 20px;
     border-top-right-radius: 20px;
-    margin-bottom: 9px;
+    word-wrap: break-word;
     word-break: keep-all;
 `;
 
@@ -41,19 +41,15 @@ export const Text = styled.p`
     font-weight: 400;
     font-size: 16px;
     line-height: 22px;
-    display: flex;
-    align-items: flex-end;
     color: #000000;
 `;
 
 export const Date = styled.p`
-    position: absolute;
-    bottom: 3px;
-    right: 49px;
+    display: inline-block;
+    vertical-align: bottom;
     font-style: normal;
     font-weight: 400;
     font-size: 10px;
     line-height: 13px;
     color: #767676;
-    margin-bottom: 9px;
 `;
