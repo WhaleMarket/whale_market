a37957aa5dc149af684f62241f54dd5f57d14dcb
“nimoseel” <fromsomini@gmail.com>
2022-07-17 14:22:50 +0900

    :sparkles: 이전페이지 이동 구현

--- src/components/notFound/NotFound.jsx
+++ src/components/notFound/NotFound.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import Icon404 from '../../assets/icon-404.png';
 
@@ -34,11 +35,12 @@ const NotFoundBtn = styled.button`
 `
 
 function NotFound() {
+    const history = useHistory();
     return (
         <NotFoundDiv>
             <NotFoundIcon src={Icon404} />
             <NotFoundSpan>페이지를 찾을 수 없습니다. :(</NotFoundSpan>
-            <NotFoundBtn>이전 페이지</NotFoundBtn>
+            <NotFoundBtn onClick={()=>history.goBack()}>이전 페이지</NotFoundBtn>
         </NotFoundDiv>
     )
 }
