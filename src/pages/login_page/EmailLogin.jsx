716fce0eefa86241399b91282fc222424864c3fd
moonhee0507 <sjmh0507@naver.com>
2022-07-17 12:57:39 +0900

    ✨ 로컬스토리지 token이 있으면 emaillogin 진입 시 /home으로 가는 기능

--- src/pages/login_page/EmailLogin.jsx
+++ src/pages/login_page/EmailLogin.jsx
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { LoginForm } from '../../components/emaillogin/LoginForm';
 
 const EmailLogin = (props) => {
+    const history = useHistory();
+
+    // 로컬스토리지에 토큰이 있으면 이메일로그인 페이지는 메인/홈으로 스킵하는 기능
+    useEffect(() => {
+        if (window.localStorage.getItem('token')) {
+            history.push('/main/home');
+        }
+    }, [history]);
+
     return (
         <LoginForm />
     );
