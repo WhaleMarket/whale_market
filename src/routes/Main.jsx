0b578ac3d6173e6de1cd7cda7f5bf1bf6836a634
soheeyeo <sohee407@gmail.com>
2022-07-19 15:34:45 +0900

    :truck: 프로필 수정 페이지 Route 추가

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -3,7 +3,10 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Chatting from "../pages/main_page/chat/Chatting";
 import ChattingView from "../pages/main_page/chat/ChattingView";
 import MainProfile from "../pages/main_page/mainProfile/MyProfile";
+<<<<<<< HEAD
 
+=======
+>>>>>>> d0db107 (:truck: 프로필 수정 페이지 Route 추가)
 import ProfileModification from "../pages/main_page/mainProfile/ProfileEdit";
 import Home from "../pages/main_page/Home";
 import Posting from "../pages/main_page/Posting";
@@ -28,7 +31,12 @@ function Test() {
           <Navbar />
           <Route path="/home" exact component={Home} />
           <Route path="/chatting" component={Chatting} />
+<<<<<<< HEAD
           <Route path="/myprofile" component={MainProfile} />
+=======
+          <Route path="/mainprofile" component={MainProfile} />
+          <Route path="/profileedit" component={ProfileModification} />
+>>>>>>> d0db107 (:truck: 프로필 수정 페이지 Route 추가)
           <Route path="/search" component={HomeSearch} />
         </>
       </Switch>
