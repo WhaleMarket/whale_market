d8659eb2a3d4bb28faee9064eee9aee9f83cf3cd
moonhee0507 <sjmh0507@naver.com>
2022-07-19 15:04:30 +0900

    💄 글로벌 스타일 a11yhidden 클래스 추가

--- src/App.js
+++ src/App.js
@@ -15,7 +15,16 @@ import NotFound from './pages/NotFoundPage';
 
 const Globalstyle = createGlobalStyle`
     ${reset}
-`
+
+    .a11yhidden {
+        position: absolute;
+        clip: rect(0 0 0 0);
+        width: 1px;
+        height: 1px;
+        margin: -1px;
+        overflow: hidden;
+    }
+`;
 
 function App() {
     return (
