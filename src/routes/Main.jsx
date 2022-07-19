fe3a9714e11a828e19f6b7e1948c1d92469a40d5
“nimoseel” <fromsomini@gmail.com>
2022-07-19 15:54:39 +0900

    :truck: HomeSearch 페이지 경로 수정

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -91,6 +91,7 @@ function MainRouter() {
           <Route path="/profileedit" component={ProfileModification} />
           <Route path="/search" component={HomeSearch} />
         </>
+
       </Switch>
     </>
   );
