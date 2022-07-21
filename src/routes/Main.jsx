70e07286cd1e2a7a1424a83541b12d71b7f2607b
soheeyeo <sohee407@gmail.com>
2022-07-21 13:49:14 +0900

    :truck: myprofile 루트 추가 및 mainprofile 루트 변경

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -19,6 +19,7 @@ function MainRouter() {
       <Switch>
         <Route path="/chatting/:chatId" component={ChattingView} />
         <Route path="/posting" component={Posting} />
+
         <Route path="/mainprofile" component={MainProfile} />
         <Route path="/profileedit" component={ProfileModification} />
         <Route path="/followers" component={Followers} />
@@ -28,6 +29,8 @@ function MainRouter() {
           <Route path="/home" exact component={Home} />
           <Route path="/chatting" component={Chatting} />
           <Route path="/myprofile" component={MainProfile} />
+          <Route path="/mainprofile" component={MainProfile} />
+          <Route path="/profileedit" component={ProfileModification} />
           <Route path="/search" component={HomeSearch} />
         </>
       </Switch>
