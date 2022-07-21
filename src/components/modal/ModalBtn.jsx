3293bdfb9c000536d9dabb0d8513188d5d6b18e6
“nimoseel” <fromsomini@gmail.com>
2022-07-21 20:04:35 +0900

    :lipstick: 모달 버튼 추가

---
+++ src/components/modal/ModalBtn.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import ModalIcon from '../../assets/icon-more-vertical.png';
+
+const ModalButton = styled.button`
+    /* className small이면 ... 이게 최선인가... */
+    position: ${(props)=>(props.className === 'small' ?  'absolute' : null )};
+    top: ${(props)=>(props.className === 'small' ?  '1.5rem' : null )};
+    right: ${(props)=>(props.className === 'small' ?  '0' : null )};
+    width : ${(props)=>(props.className === 'small' ?  '1.125rem' : '1.5rem' )};
+    height : ${(props)=>(props.className === 'small' ?  '1.125rem' : '1.5rem' )};
+    border: none;
+    background-color: inherit;
+    background-image: url(${ModalIcon});
+    background-size : ${(props)=>(props.className === 'small' ?  '1.125rem 1.125rem' : '1.375rem 1.375rem' )};
+    &:hover{
+        cursor: pointer;
+    }
+`;
+
+function ModalBtn({className, onClick}) {
+    return <ModalButton className={className} onClick={onClick}/>
+}
+
+export default ModalBtn;
