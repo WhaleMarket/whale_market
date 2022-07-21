2514330a4c35466e417de08aab3a1f9c4c64a6b0
soheeyeo <sohee407@gmail.com>
2022-07-21 15:18:42 +0900

    :truck: mainprofile, myprofile 루트 변경

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Chatting from "../pages/main_page/chat/Chatting";
 import ChattingView from "../pages/main_page/chat/ChattingView";
 import MainProfile from "../pages/main_page/mainProfile/MyProfile";
+
 import ProfileModification from "../pages/main_page/mainProfile/ProfileEdit";
 import Home from "../pages/main_page/Home";
 import Posting from "../pages/main_page/Posting";
@@ -18,7 +19,7 @@ function Test() {
       <Switch>
         <Route path="/chatting/:chatId" component={ChattingView} />
         <Route path="/posting" component={Posting} />
-        <Route path="/myprofile" component={MainProfile} />
+        <Route path="/mainprofile" component={MainProfile} />
         <Route path="/profileedit" component={ProfileModification} />
         <Route path="/followers" component={Followers} />
         <Route path="/productupload" component={ProductUpload} />
@@ -27,7 +28,7 @@ function Test() {
           <Navbar />
           <Route path="/home" exact component={Home} />
           <Route path="/chatting" component={Chatting} />
-          <Route path="/mainprofile" component={MainProfile} />
+          <Route path="/myprofile" component={MainProfile} />
           <Route path="/search" component={HomeSearch} />
         </>
       </Switch>
