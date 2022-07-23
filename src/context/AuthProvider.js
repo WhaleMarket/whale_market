e324ed2701e204d4f5bbe399af55f63d625fe914
moonhee0507 <sjmh0507@naver.com>
2022-07-23 15:25:20 +0900

    ✨ 유저 정보 연결(토큰, 유저네임, 팔로워 등)

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -4,9 +4,38 @@ const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState({});
-
+    const [token, setToken] = useState(localStorage.getItem('token'));
+    const [myImage, setMyImage] = useState('');
+    const [myUsername, setMyUsername] = useState('');
+    const [myAccountname, setMyAccountname] = useState(
+        localStorage.getItem('accountname')
+        );
+    const [myIntro, setMyIntro] = useState('');
+    const [myFollowerCount, setMyFollowerCount] = useState('');
+    const [myFollowingCount, setMyFollowingCount] = useState('');
+    const [myFollowerList, setMyFollowerList] = useState([]);
+        
     return (
-        <AuthContext.Provider value={{ auth, setAuth }}>
+        <AuthContext.Provider value={{ 
+            auth, 
+            setAuth,
+            token,
+            setToken,
+            myImage,
+            setMyImage,
+            myUsername,
+            setMyUsername,
+            myAccountname,
+            setMyAccountname,
+            myIntro,
+            setMyIntro,
+            myFollowerCount,
+            setMyFollowerCount,
+            myFollowingCount,
+            setMyFollowingCount,
+            myFollowerList,
+            setMyFollowerList
+        }}>
             {children}
         </AuthContext.Provider>
     )
