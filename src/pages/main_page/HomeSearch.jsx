91d742c67c6e0b662b082cc0bda8148cb0ddb9db
moonhee0507 <sjmh0507@naver.com>
2022-07-25 01:11:49 +0900

    ✨ 검색 결과 받아오기 및 props 전달 적용

--- src/pages/main_page/HomeSearch.jsx
+++ src/pages/main_page/HomeSearch.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import SearchBar from '../../components/main/homeSearch/SearchBar';
 import Navbar from '../../components/main/Navbar';
+import SearchList from '../../components/main/homeSearch/SearchList';
+import axios from 'axios';
+import { API_URL } from '../../constants/defaultUrl';
+import AuthContext from '../../context/AuthProvider';
 
 function HomeSearch(){
+    const [searchResult, setSearchResult] = useState([]);
+    const [keyword, setKeyword] = useState('');
+    const [InfoState]  = useContext(AuthContext);
+
+    function handleKeyword(event) {
+        setKeyword(event.target.value);
+        if (event.target.value === '') {
+            setSearchResult([]);
+        }
+    }
+
+    useEffect(() => {
+        if (keyword) {
+            const search = async () => {
+                    const token = window.localStorage.getItem('token');
+                    const config = {
+                        headers: {
+                            "Authorization" : `Bearer ${token}`,
+                            "Content-Type": "application/json",
+                        },
+                    };
+                    const response = await axios.get(
+                        `${API_URL}/user/searchuser/?keyword=${keyword}`,
+                        config
+                    );
+                    setSearchResult(response.data);
+            };
+            search();
+        }
+    }, [keyword]);
+
     return(
         <>
-            <SearchBar />
+            <SearchBar value={keyword} handleKeyword={handleKeyword} />
+            <SearchList searchResult={searchResult} />
             <Navbar />
         </>
     );
