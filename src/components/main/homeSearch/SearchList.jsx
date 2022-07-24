cf350ea4e91c46669a5099b38fdff1d949c74df2
moonhee0507 <sjmh0507@naver.com>
2022-07-24 21:15:56 +0900

    💄 검색 컴포넌트 UI 구현

---
+++ src/components/main/homeSearch/SearchList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SearchListItem from './SearchListItem';
+
+function SearchList() {
+    return (
+        <SearchListItem />
+    )
+}
+
+export default SearchList;
