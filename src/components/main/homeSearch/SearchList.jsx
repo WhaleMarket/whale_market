564c8955b0d50eedb59cd88ee99921a334c17e18
moonhee0507 <sjmh0507@naver.com>
2022-07-25 15:46:05 +0900

    ✨ 유저 이미지 URL 정규표현식으로 예외처리 및 키워드 강조 기능

--- src/components/main/homeSearch/SearchList.jsx
+++ src/components/main/homeSearch/SearchList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import basicProfileImage from '../../../assets/basic-profile-img.png';
 
 const SearchUl = styled.ul`
     padding: 68px 16px 0 16px;
@@ -26,12 +27,34 @@ const UserProfileImage = styled.div`
     border-radius: 50%;
 `;
 
+const TextBox = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const UsernameBox = styled.div`
+    display: flex;
+`;
+
 const UserName = styled.p`
     font-style: normal;
     font-weight: 500;
     font-size: 14px;
     line-height: 18px;
     margin-bottom: 6px;
+`;
+
+const StyledKeywordInUsername = styled.p`
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 18px;
+    margin-bottom: 6px;
+    color: #00BCD4;
+`;
+
+const AccountNameBox = styled.div`
+    display: flex;
 `;
 
 const AccountName = styled.p`
@@ -42,18 +65,52 @@ const AccountName = styled.p`
     color: #767676;
 `;
 
-function SearchList({ searchResult }) {
+const StyledKeywordInAccountname = styled.p`
+    font-style: normal;
+    font-weight: 700;
+    font-size: 12px;
+    line-height: 15px;
+    color: #00BCD4;
+`;
+
+
+function SearchList({ keyword, searchResult }) {
+    const imageRegex = new RegExp(/(^https:\/\/mandarin.api.weniv.co.kr\/)[1-9]+\.[a-zA-Z]+$/g);
+
     return (
         <SearchUl>
             {searchResult.map((user) => {
+                
                 return (
                     <List key={user._id}>
                         <FlexedLink to={`/${user.accountname}`}>
-                            <UserProfileImage style={{ backgroundImage: `url(${user.image})` }}/>
-                            <div>
-                                <UserName>{user.username}</UserName>
-                                <AccountName>@{user.accountname}</AccountName>
-                            </div>
+                            {imageRegex.test(user.image) ? (
+                                <UserProfileImage style={{ backgroundImage: `url(${user.image})` }}/>
+                            ) : (
+                                <UserProfileImage style={{ backgroundImage: `url(${basicProfileImage})` }}/>
+                            )}
+                                <TextBox>
+                                {user.username.includes(keyword) ? (
+                                    <UsernameBox>
+                                        <UserName>{user.username.split(keyword)[0]}</UserName>
+                                        <StyledKeywordInUsername>{keyword}</StyledKeywordInUsername>
+                                        <UserName>{user.username.split(keyword)[1]}</UserName>
+                                    </UsernameBox>
+                                ) : (
+                                    <UserName>{user.username}</UserName>
+                                )}
+                            
+                                {user.accountname.includes(keyword) ? (
+                                    <AccountNameBox>
+                                        <AccountName>@{user.accountname.split(keyword)[0]}</AccountName>
+                                        <StyledKeywordInAccountname>{keyword}</StyledKeywordInAccountname>
+                                        <AccountName>{user.accountname.split(keyword)[1]}</AccountName>
+                                    </AccountNameBox>
+                                ) : (
+                                        <AccountName>@{user.accountname}</AccountName>
+                                )}
+                                </TextBox>
+
                         </FlexedLink>
                     </List>
                 )
