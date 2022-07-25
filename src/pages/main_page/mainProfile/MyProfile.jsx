e844c2516f4c94ff0bc6469e3cbbc0d087a6b20b
Lim Heelae <ehhwll@hanmail.net>
2022-07-25 12:28:28 +0900

    :lipstick: 상품이 없을 때 상품 판매 부분이 나오지 않도록 변경

--- src/pages/main_page/mainProfile/MyProfile.jsx
+++ src/pages/main_page/mainProfile/MyProfile.jsx
@@ -2,13 +2,16 @@ import ChatProfileHeader from "../../../components/main/ChatProfileHeader";
 import UserProfileSection from "../../../components/main/mainProfile/user_profile/UserProfileSection";
 import ProductSection from "../../../components/main/mainProfile/user_profile/ProductSection";
 import PostSection from "../../../components/main/mainProfile/user_profile/PostSection";
+import { useContext } from "react";
+import AuthContext from "../../../context/AuthProvider";
 
 function MainProfile() {
+  const [InfoState] = useContext(AuthContext);
   return (
     <>
       <ChatProfileHeader />
       <UserProfileSection />
-      <ProductSection />
+      {InfoState.MyInformations[2].itemName.length !== 0 && <ProductSection />}
       <PostSection />
     </>
   );
