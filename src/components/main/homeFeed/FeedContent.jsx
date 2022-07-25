bb91b3eaa823773b85d82b1e1e892108513f314d
Lim Heelae <ehhwll@hanmail.net>
2022-07-25 12:20:04 +0900

    :recycle: useContext를 이용한 코드 리팩토링

--- src/components/main/homeFeed/FeedContent.jsx
+++ src/components/main/homeFeed/FeedContent.jsx
@@ -5,7 +5,7 @@ import IconGroup from "./IconGroup";
 import AuthContext from "../../../context/AuthProvider";
 
 const LayOut = styled.ul`
-  padding: 53px 0 56px;
+  padding: 53px 0 68px 0;
   /* position: fixed; */
   display: flex;
   flex-direction: column;
@@ -13,17 +13,18 @@ const LayOut = styled.ul`
 `;
 
 const FeedWrapper = styled.li`
+  border: 1px solid black;
+  border-radius: 10px;
   display: inline-block;
   /* width: calc(100% - 32px); */
   min-width: 358px;
   margin: 20px 16px 0;
   display: flex;
   flex-direction: column;
+  padding: 10px;
 `;
 
-const ContentWrapper = styled.div`
-  margin-left: 54px;
-`;
+const ContentWrapper = styled.div``;
 
 const ContentText = styled.p`
   margin: 12px 0 16px 0;
@@ -34,11 +35,17 @@ const ContentText = styled.p`
 
 const ContentImg = styled.img`
   overflow: hidden; //
-  max-width: 303px;
+  max-width: 360px;
   min-height: 228px;
   border-radius: 10px;
   object-fit: cover;
-  display: block;
+  margin-bottom: 10px;
+`;
+
+const ImgWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const CreatedDate = styled.p`
@@ -67,14 +74,16 @@ function FeedContent() {
           />
           <ContentWrapper>
             <ContentText>{InfoState.MyInformations[5].content[i]}</ContentText>
-            {InfoState.MyInformations[5].image[i] &&
-              InfoState.MyInformations[5].image[i]
-                .split(",")
-                .map((value, key) => {
-                  return (
-                    <ContentImg key={key} src={value} alt="게시글 이미지" />
-                  );
-                })}
+            <ImgWrapper>
+              {InfoState.MyInformations[5].image[i] &&
+                InfoState.MyInformations[5].image[i]
+                  .split(",")
+                  .map((value, key) => {
+                    return (
+                      <ContentImg key={key} src={value} alt="게시글 이미지" />
+                    );
+                  })}
+            </ImgWrapper>
             <IconGroup
               id={InfoState.MyInformations[5].id[i]}
               like={InfoState.MyInformations[5].heartCount[i]}
