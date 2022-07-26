228e64c84a4a1f1d91bf93899866c1608836303f
moonhee0507 <sjmh0507@naver.com>
2022-07-26 13:16:54 +0900

    🩹 AuthContext 추가(중간)

--- src/components/main/Navbar.jsx
+++ src/components/main/Navbar.jsx
@@ -8,6 +8,9 @@ import posting_icon from "../../assets/icon-edit.png";
 import user_icon_fill from "../../assets/icon-user-fill.png";
 import user_icon from "../../assets/icon-user.png";
 
+import AuthContext from "../../context/AuthProvider";
+import { useContext } from "react";
+
 const Bottomnav = styled.ul`
   display: flex;
   justify-content: space-around;
@@ -41,12 +44,14 @@ const Logo = styled.img`
 `;
 
 const Navbar = ({location})=>{
+    const [InfoState] = useContext(AuthContext);
     return(
         <Bottomnav>
             <List className='home' current={location.pathname.substr(0, 5) === '/home'}><Link to='/home'><Logo className="Home" src={location.pathname.substr(0, 5) === '/home' ? home_icon_fill : home_icon} alt="home icon"/>홈</Link></List>
             <List className='chatting' current={location.pathname === '/chatting'}><Link to='/chatting'><Logo className="Home" src={location.pathname === '/chatting' ? chatting_icon_fill : chatting_icon} alt="home icon"/>채팅</Link></List>
             <List className='posting' current={location.pathname === '/posting'}><Link to='/posting'><Logo className="Home" src={posting_icon} alt="home icon"/>게시물 작성</Link></List>
-            <List className='profile' current={location.pathname === '/myprofile'}><Link to='/myprofile'><Logo className="Home" src={location.pathname === '/myprofile' ? user_icon_fill : user_icon} alt="home icon"/>프로필</Link></List>
+            {/* <List className='profile' current={location.pathname === '/myprofile'}><Link to='/myprofile'><Logo className="Home" src={location.pathname === '/myprofile' ? user_icon_fill : user_icon} alt="home icon"/>프로필</Link></List> */}
+            <List className='profile' current={location.pathname === `/profile/${InfoState.MyInformations[0].myAccountname}`}><Link to={`/profile/${InfoState.MyInformations[0].myAccountname}`}><Logo className="Home" src={location.pathname === `/profile/${InfoState.MyInformations[0].myAccountname}` ? user_icon_fill : user_icon} alt="home icon"/>프로필</Link></List>
         </Bottomnav>
     )
 }
